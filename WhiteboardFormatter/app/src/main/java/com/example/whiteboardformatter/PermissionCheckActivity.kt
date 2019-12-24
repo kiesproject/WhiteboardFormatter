0b285c0c30bd80e4779aac3ba3492bb54677b774
YamamotoKazuma <b1917206@planet.kanazawa-it.ac.jp>
2019-12-24 20:24:15 +0900

    PermissionCheckActivityの追加

---
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/PermissionCheckActivity.kt
@@ -0,0 +1,12 @@
+package com.example.whiteboardformatter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PermissionCheckActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission_check)
+    }
+}
