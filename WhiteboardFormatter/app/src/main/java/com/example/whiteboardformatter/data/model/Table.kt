cd899502d8da4f24d24995947027a734f6b94dd1
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2020-01-03 22:41:54 +0900

    Table_Entity

---
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/model/Table.kt
@@ -0,0 +1,26 @@
+package com.example.whiteboardformatter.data.model
+
+import androidx.room.*
+import java.util.*
+
+@Entity(
+data class Whiteboard constructor(
+        @PrimaryKey(autoGenerate = true)
+        var id : Long=0,
+        val timestump : Long = Date().time
+)
+data class Text constructor(
+        @PrimaryKey(autoGenerate = true)
+        var id: Long = 0,
+        var whiteboard: Long,
+        var text: String = "",
+        var x: Integer,
+        var y: Integer,
+        var width: Integer,
+        var height: Integer
+)
+)
+
+
+
+
