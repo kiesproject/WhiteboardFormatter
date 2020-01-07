2f444d85ba40e28cd8bced84bbbf32d12f7d5135
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2020-01-07 19:11:33 +0900

    DB_Dao
    修正しました

--- WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/dao/Dao.kt
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/dao/Dao.kt
@@ -8,24 +8,24 @@ import com.example.yabamiru.data.model.TaskAndTaskTags
 @Dao
 interface TableDao{
 
-    @Query("SELECT * FROM Table")
-    fun getAll():LiveData<List<Table>>
+    @Query("SELECT * FROM whiteboard")
+    fun getAll():LiveData<List>
 
-    @Query("SELECT * FROM Table WHERE whiteboardId=(:whiteboardId)")
-    fun getById(whiteboardId:Long):LiveData<Task>
-
-    @Insert
-    fun insert(vararg text:text):Text<String>
+    @Query("SELECT * FROM Text WHERE whiteboardId=(:whiteboardId)")
+    fun getById(whiteboardId:Long):LiveData<List>
 
     @Insert
-    fun insert(vararg whiteboard:text):Whiteboard<String>
+    fun insert(UpperCamel text:Text)
+
+    @Insert
+    fun insert(Whiteboard:text):Whiteboard<String>
 
     @Update
-    fun update(text:text)
+    fun update(UpperCamel text:Text)
 
-    @Query("DELETE FROM Table WHERE whiteboard=(:Whiteboard)")
-    fun delete(Whiteboard:String)
+    @Query("DELETE FROM Table WHERE whiteboardId=(:WhiteboardId)")
+    fun delete(WhiteboardId:String)
 
-    @Query("DELETE FROM Table WHERE text=(:Text)")
-    fun delete(Text:String)
+    @Query("DELETE FROM Table WHERE id=(:textId)")
+    fun delete(textId:Long)
 }
