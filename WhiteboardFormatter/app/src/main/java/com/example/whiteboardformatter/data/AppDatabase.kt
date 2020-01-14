9a47af5173a76083e323b0b72c31e91615793cc7
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2020-01-14 18:19:18 +0900

    AppDatabase
    DBの設計のプログラムです

--- WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/AppDatabase.kt
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/AppDatabase.kt
@@ -1,2 +1,38 @@
 package com.example.whiteboardformatter.data
 
+import androidx.room.*
+import android.content.Context
+import android.graphics.Color
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+
+@Database(
+        entities = arrayOf(
+                Whiteboard::class
+                        Text::class
+        ),version = 1
+)
+abstract class AppDatabase:RoomDatabase(){
+    abstract fun dao():Dao()
+
+    companion object {
+        @Volatile
+        private var INSTANCE:AppDatabase? = null
+
+        fun getDatabase(context:Context):AppDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabas::class.java,
+                        "task_database"
+                )
+            }
+        }
+    }
+}
+
