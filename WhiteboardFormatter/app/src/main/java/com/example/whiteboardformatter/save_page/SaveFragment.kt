4816139471f13d615193fda10bde224bb3017136
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2020-01-24 00:10:23 +0900

    add func for update markdown preview when it changed

--- WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/save_page/SaveFragment.kt
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/save_page/SaveFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.whiteboardformatter.data.model.TextForPreview
 import com.example.whiteboardformatter.databinding.FragmentSaveBinding
 import com.example.whiteboardformatter.util.getViewModelFactory
+import io.noties.markwon.Markwon
 
 class SaveFragment :Fragment(){
     private val viewModel:SaveViewModel by viewModels { getViewModelFactory() }
@@ -19,5 +22,28 @@ class SaveFragment :Fragment(){
             lifecycleOwner = viewLifecycleOwner
         }
         return fragmentSaveBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val markwon = Markwon.create(view.context)
+        setMdText(markwon)
+
+        //仮置きデータ
+        val textArray = arrayOf(
+            TextForPreview("最初のテキスト",1000,0,100,200,1f,1f),
+            TextForPreview("二番目のテキスト",100,1000,100,200,1f,1f)
+        )
+
+        viewModel.start(textArray)
+
+    }
+
+    private fun setMdText(markwon:Markwon){
+        viewModel.previewMdText.observe(this.viewLifecycleOwner, Observer {
+            val markdown = markwon.toMarkdown(it)
+            markwon.setParsedMarkdown(fragmentSaveBinding.saveMdPreviewTextview,markdown)
+        })
     }
 }
