642d7c8c7da85aaf0ec8f3fb729eafdbfc76dd67
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2020-01-24 23:55:29 +0900

    navigation safeArgsで使えるように変更

--- WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/model/TextForEdit.kt
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/model/TextForEdit.kt
@@ -1,7 +1,11 @@
 package com.example.whiteboardformatter.data.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class TextForEdit(
     val text:String,
     val x:Int,
     val y:Int
-)
+):Parcelable
