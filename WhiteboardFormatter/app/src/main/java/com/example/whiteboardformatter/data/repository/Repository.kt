5f5be427093bc67a6299074665a766dd2c544c22
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2020-01-24 18:17:01 +0900

    DB_repository

--- WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/repository/Repository.kt
+++ WhiteboardFormatter/app/src/main/java/com/example/whiteboardformatter/data/repository/Repository.kt
@@ -4,18 +4,18 @@ import com.example.whiteboardformatter.data.dao.TextDao
 
 class Repository(textDao:TextDao){
     fun getAll():List<Whiteboard>{
-        return tableDao.getAll()
+        return textDao.getAll()
     }
     fun getById(whiteboardId:Long):Table{
-        return tableDao.getById(whiteboardId)
+        return textDao.getById(whiteboardId)
     }
     fun insert(text:Text){
-        tableDao.insert(text)
+        textDao.insert(text)
     }
     fun insert(whiteboard:Whiteboard){
-        tableDao.insert(whiteboard)
+        textDao.insert(whiteboard)
     }
     fun update(text:Text){
-        tableDao.update(text)
+        textDao.update(text)
     }
 }
